5d3d40e5e5a599eee6969fc2b9cec89b2649fa51
Gonzalo Muñoz Ortega <gonzodl38@gmail.com>
2026-05-17 16:25:06 -0500

    dsbsdfdfg

---
+++ backend/models/base_model.py
@@ -0,0 +1,2 @@
+from extensions import db
+from datetime import datetime
--- backend/models/dependency.py
+++ backend/models/dependency.py
@@ -1,3 +1,6 @@
+from models.base_model import BaseModel
+from extensions import db
+
 class Dependency(BaseModel, db.Model):
     __tablename__ = "dependencies"
 
